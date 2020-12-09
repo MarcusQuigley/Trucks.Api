a53c1237208809ada723c448a3a5dde817de4c63
marcus quigley <marcusquigley@gmail.com>
2020-12-09 18:43:00 -0500

    setting required table columns

--- Trucks.Api.DataAccess/Data/ApplicationDbContext.cs
+++ Trucks.Api.DataAccess/Data/ApplicationDbContext.cs
@@ -15,11 +15,24 @@ namespace Trucks.Api.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Truck>(entity => {
-            //    entity.Property(t => t.Name).IsRequired();
-            //    entity.Property(t => t.Description).IsRequired();
-            //    entity.Property(t => t.Price).IsRequired();
-            //});
+            modelBuilder.Entity<Truck>(entity => {
+                entity.Property(t => t.Name).IsRequired();
+                entity.Property(t => t.Description).IsRequired();
+                entity.Property(t => t.Price).IsRequired();
+            });
+
+            modelBuilder.Entity<Photo>(entity => {
+                entity.Property(t => t.PhotoPath).IsRequired();
+            });
+
+            modelBuilder.Entity<Category>(entity => {
+                entity.Property(t => t.Name).IsRequired();
+            });
+
+            modelBuilder.Entity<TruckInventory>(entity => {
+                entity.Property(t => t.Quantity).IsRequired();
+            });
+
             //compositeKey
             modelBuilder.Entity<TruckCategory>().HasKey(tc => new { tc.CategoryId, tc.TruckId });
 
---
+++ Trucks.Api.DataAccess/Migrations/20201209234129_settingRequiredColumns.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trucks.Api.DataAccess.Migrations
+{
+    public partial class settingRequiredColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Trucks",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Trucks",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoPath",
+                table: "Photo",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Trucks",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Trucks",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhotoPath",
+                table: "Photo",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
